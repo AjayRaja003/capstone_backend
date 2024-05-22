aa27156fee32005ab123168a03306e31565907df
AJAY <ajay.raja0903@gmail.com>
2024-05-22 10:55:00 +0530

    commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ import { User } from "../models/userSchema.js";
 import { catchAsyncErrors } from "./catchAsyncErrors.js";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.header('Authorization');
+  const token  = req.headers['authorization'];
   if (!token) {
     return next(new ErrorHandler("User is not authenticated!", 400));
   }
